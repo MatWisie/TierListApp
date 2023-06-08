bc5b4c02f32b74c3d769d4607152baae015a2cfd
MatWisie <Mateuszwisiecki@gmail.com>
2023-06-08 14:11:13 +0200

    Added logic and controls for AddTierView

--- TierListApp/ViewModels/AddTierListViewModel.cs
+++ TierListApp/ViewModels/AddTierListViewModel.cs
@@ -1,13 +1,35 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TierListApp.Models;
 
 namespace TierListApp.ViewModels
 {
-    public class AddTierListViewModel : ObservableObject
+    partial class AddTierListViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string tierListName = "LALAL";
+
+        [ObservableProperty]
+        private ObservableCollection<Tier> listOfTiers = new ObservableCollection<Tier>();
+
+
+        [RelayCommand]
+        public void AddNewTier()
+        {
+            Tier tmpTier = new Tier
+            {
+                Id = 1,
+                Name = "S",
+                TierListId = 1
+            };
+
+            ListOfTiers.Add(tmpTier);
+        }
     }
 }
